c245721e45314354d9d2a299ade336520d170155
Carson Holland <cnh1116@gmail.com>
2025-09-26 16:13:42 -0500

    Made hello.cpp actually a true hello.cpp

--- hello/hello.cpp
+++ hello/hello.cpp
@@ -4,13 +4,5 @@ int main()
 {
     std::cout << "Hello, world!" << std::endl;
 
-    u_int x = 2;
-
-    for(u_int i = 0; i < 32; i++)
-    {
-        x *= 2;
-        std::cout << x << std::endl;
-    }
-
     return 0;
 }
